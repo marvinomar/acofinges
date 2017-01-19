db20d73e25b4893c6a8f5048ad35b90568774774
mpenate <mpenate@salud.gob.sv>
2017-01-19 13:55:20 -0600

    finalizando logica de ambos flujos

--- src/AcofingesAdministracionBundle/Resources/public/bootzard-bootstrap-wizard-template/assets/js/scripts2.js
+++ src/AcofingesAdministracionBundle/Resources/public/bootzard-bootstrap-wizard-template/assets/js/scripts2.js
@@ -20,24 +20,20 @@ function bar_progress(progress_line_object, direction) {
 }
 
 jQuery(document).ready(function() {
-	
     /*
         Fullscreen background
     */
     $.backstretch("assets/img/backgrounds/1.jpg");
-    
     $('#top-navbar-1').on('shown.bs.collapse', function(){
     	$.backstretch("resize");
     });
     $('#top-navbar-1').on('hidden.bs.collapse', function(){
     	$.backstretch("resize");
     });
-    
     /*
         Form
     */
     $('.f1 fieldset:first').fadeIn('slow');
-    
     $('.f1 input[type="text"], .f1 input[type="password"], .f1 textarea, .f1 input[type="radio"]').on('focus', function() {
     	$(this).removeClass('input-error');
     });
@@ -45,6 +41,30 @@ jQuery(document).ready(function() {
          $('input[name=tipo]').parent().removeClass('input-error');
          $('input[name=empleo]').parent().removeClass('input-error');
          $('input[name=ingresos]').parent().removeClass('input-error');
+         $('input[name=codeudor]').parent().removeClass('input-error');
+    });
+    function validarEmail( email ) {
+        expr = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+        if ( !expr.test(email) ){
+            return 0; /*Email incorrecto*/
+        }else{
+            return 1;/*Email correcto*/
+        }
+    }
+   /* function tipoIngreso(){
+        var tipoIngresos=$('input:radio[name="ingresos"]').val();
+        return tipoIngresos;
+    }*/
+    $('input:radio[name="codeudor"]').change(
+    function(){
+        if (this.checked && this.value == 'si') {
+            $("#parentesco").show("slow");
+            $("#ingresosCodeudor").show("slow");
+        }
+        if (this.checked && this.value == 'no') {
+            $("#parentesco").hide("slow");
+            $("#ingresosCodeudor").hide("slow");
+        }
     });
     // next step
     $('.f1 .btn-next').on('click', function() {
@@ -53,32 +73,19 @@ jQuery(document).ready(function() {
     	// navigation steps / progress steps
     	var current_active_step = $(this).parents('.f1').find('.f1-step.active');
     	var progress_line = $(this).parents('.f1').find('.f1-progress-line');
-    	
     	// fields validation
-        // 
-    	parent_fieldset.find('input[type="text"], input[type="password"], textarea').each(function() {
-            if(current_active_step.attr('id')=='paso5'){
-                if(parent_fieldset.find('input[id=si]:checked').val()){
-    		      if( $(this).val() == "" ) {
-    			     $(this).addClass('input-error');
-    			     next_step = false;
-    		      }
-    		      else {
-    			     $(this).removeClass('input-error');
-    		      }
-                }else{
-                    $(this).removeClass('input-error');
-                }
-                }else{
-                    if( $(this).val() == "") {
-                        $(this).addClass('input-error');
-                        next_step = false;
-                    }
-                    else {
-                        $(this).removeClass('input-error');
-                    }
-            }
-    	});
+        //
+        if(current_active_step.attr('id')!='paso5'){
+            	parent_fieldset.find('input[type="text"], input[type="password"], textarea').each(function() {
+                            if( $(this).val() == "") {
+                                $(this).addClass('input-error');
+                                next_step = false;
+                            }
+                            else {
+                                $(this).removeClass('input-error');
+                            }
+            	});
+        }
         if(current_active_step.attr('id')=='paso1'){
             if (!parent_fieldset.find('input[name=tipo]:checked').val() ) {
                 $('input[name=tipo]').parent().addClass('input-error');
@@ -90,6 +97,22 @@ jQuery(document).ready(function() {
 
      }
      if(current_active_step.attr('id')=='paso2'){
+        if(validarEmail(parent_fieldset.find('input[id=f1-correo]').val())==0){
+            BootstrapDialog.show({
+                title: '<h4>Advertencia</h4>',
+                message: '<h5 class="text-danger">El correo ingresado no es valido, debe ingresarlo con el formato ####@####.###</h5>',
+                type: BootstrapDialog.TYPE_DANGER,
+                buttons: [{
+                label: 'Ok',
+                cssClass: 'btn-danger btn-previous',
+                action: function(dialogItself){
+                    dialogItself.close();
+                }
+            }]
+            });
+            return false;
+        }
+
         if ((parent_fieldset.find('input[id=f1-telefono]').val()).length < 9 ) {
             BootstrapDialog.show({
                 title: '<h4>Advertencia</h4>',
@@ -151,7 +174,7 @@ jQuery(document).ready(function() {
             });
             return false;
          }
-     }   
+     }
         if(current_active_step.attr('id')=='paso3'){
         /*Agregando validacion de tiempo de edad*/
         if (parent_fieldset.find('input[id=f1-tiempo]').val() <= 6 ) {
@@ -177,7 +200,7 @@ jQuery(document).ready(function() {
                 $('input[name=empleo]').parent().removeClass('input-error');
             }
         }
-        if(current_active_step.attr('id')=='paso5'){
+        if(current_active_step.attr('id')=='paso4'){
             if (!parent_fieldset.find('input[name=ingresos]:checked').val() ) {
                 $('input[name=ingresos]').parent().addClass('input-error');
                 next_step = false;
@@ -185,22 +208,52 @@ jQuery(document).ready(function() {
             else {
                 $('input[name=ingresos]').parent().removeClass('input-error');
             }
-            /*Realizando cálculos*/
-            var valor = $("input[name='f1-ingresos']").val();
-            if (parent_fieldset.find('input[id=sal]:checked').val() ) {
-                var monto =valor*12;
-                $("#td1").html(Math.round(monto*100)/100);
-            }else if(parent_fieldset.find('input[id=neg]:checked').val() ){
-                var monto =valor*9;
-                $("#td1").html(Math.round(monto*100)/100);
-            }else{
-                var monto =valor*10;
-                $("#td1").html(Math.round(monto*100)/100);
-            }
         }
 
-    	// fields validation
+        if(current_active_step.attr('id')=='paso5'){
+            if (!parent_fieldset.find('input[name=codeudor]:checked').val() ) {
+                $('input[name=codeudor]').parent().addClass('input-error');
+                next_step = false;
+            }
+            else {
+                $('input[name=codeudor]').parent().removeClass('input-error');
+            }
 
+            if(parent_fieldset.find('input[id=si]:checked').val()){
+            parent_fieldset.find('input[type="text"], input[type="password"], textarea').each(function() {
+                if( $(this).val() == "") {
+                    $(this).addClass('input-error');
+                    next_step = false;
+                }
+                else {
+                    $(this).removeClass('input-error');
+                }
+            });
+            }else{
+                 $(this).removeClass('input-error');
+            }
+            /*Realizando cálculos*/
+            var ingresos = $("input[name='f1-ingresos']").val();
+            var otros = $("input[name='f1-otros']").val();
+            var ingresoTotal=parseInt(ingresos)+parseInt(otros);
+            if ($('input:radio[name="ingresos"]:checked').val()=='sal') {
+                var monto1 =parseInt(ingresoTotal)*45;
+            }else if($('input:radio[name="ingresos"]:checked').val()=='hon'){
+                var monto1 =parseInt(ingresoTotal)*35;
+            }else{
+                var monto1 =parseInt(ingresoTotal)*40;
+            }
+            //Calculos codeudor
+            if (parent_fieldset.find('input[id=si]:checked').val() ) {
+                var ingresosCodeudor = $("input[name='f1-ingresosCodeudor']").val();
+                var monto2=(parseInt(ingresosCodeudor)*45)-parseInt(monto1);
+            }else{
+                var monto2=0;
+            }
+            var montoMax=parseInt(monto1)+parseInt(monto2);
+            $("#td1").html(Math.round(montoMax*100)/100);
+        }
+    	// fields validation
     	if( next_step ) {
     		parent_fieldset.fadeOut(400, function() {
     			// change icons
@@ -213,9 +266,7 @@ jQuery(document).ready(function() {
     			scroll_to_class( $('.f1'), 20 );
 	    	});
     	}
-    	
     });
-    
     // previous step
     $('.f1 .btn-previous').on('click', function() {
     	// navigation steps / progress steps
@@ -224,6 +275,7 @@ jQuery(document).ready(function() {
     	$('input[name=tipo]').parent().removeClass('input-error');
         $('input[name=empleo]').parent().removeClass('input-error');
         $('input[name=ingresos]').parent().removeClass('input-error');
+        $('input[name=codeudor]').parent().removeClass('input-error');
     	$(this).parents('fieldset').fadeOut(400, function() {
     		// change icons
     		current_active_step.removeClass('active').prev().removeClass('activated').addClass('active');
